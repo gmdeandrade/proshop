a6c3903b6ebcf746538c2f052d3ecc3bb15059bf
gbr <gabrielmonteiro.adr@gmail.com>
2023-07-21 21:48:04 -0300

    product screen

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -3,13 +3,15 @@ import {
   createRoutesFromElements,
   Route,
 } from "react-router-dom";
-import HomeScreen from "./screens/HomeScreen";
 import App from "./App";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
     </Route>
   )
 );
