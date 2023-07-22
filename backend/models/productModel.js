defb3862a5902f90517d217fd1d216f98ce3d889
gbr <gabrielmonteiro.adr@gmail.com>
2023-07-22 20:11:33 -0300

    seeder (import&destroy)

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -19,6 +19,11 @@ const reviewSchema = mongoose.Schema(
 
 const productSchema = mongoose.Schema(
   {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
     name: {
       type: String,
       required: true,
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,10 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: {
-      type: String,
-      required: true,
-    },
+    email: { type: String, required: true },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, required: true, default: false },
   },
