e880e8d2dd657ff959c76f2bcdf9a19dcda0915f
gbr <gabrielmonteiro.adr@gmail.com>
2023-07-26 13:45:31 -0300

    Cart Screen

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -14,7 +14,7 @@ const cartSlice = createSlice({
       const existItem = state.cartItems.find((x) => x._id === item._id);
       if (existItem) {
         state.cartItems = state.cartItems.map((x) =>
-          x._id === existItem._id ? { ...item, qty: x.qty + item.qty } : x
+          x._id === existItem._id ? item : x
         );
       } else {
         state.cartItems = [...state.cartItems, item];
@@ -22,8 +22,13 @@ const cartSlice = createSlice({
 
       return updateCart(state);
     },
+
+    removeFromCart: (state, action) => {
+      state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
+      return updateCart(state);
+    },
   },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart } = cartSlice.actions;
 export default cartSlice.reducer;
