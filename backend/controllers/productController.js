1ae27bb4e24d74e2c1ed6e2a1960c5f4a12ebecb
gbr <gabrielmonteiro.adr@gmail.com>
2023-08-04 10:37:55 -0300

    create new product logic

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -10,6 +10,7 @@ const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({});
   res.json(products);
 });
+
 /**
  * @desc    Fetch single product
  * @route   GET /api/products/:id
@@ -24,4 +25,26 @@ const getProductById = asyncHandler(async (req, res) => {
   res.json(product);
 });
 
-export { getProducts, getProductById };
+/**
+ * @desc    Create a product
+ * @route   POST /api/products
+ * @access  Private/Admin
+ */
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample name",
+    price: 0,
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    brand: "Sample brand",
+    category: "Sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample description",
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+export { getProducts, getProductById, createProduct };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 import {
   getProducts,
   getProductById,
+  createProduct,
 } from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
-router.route("/").get(getProducts);
+router.route("/").post(protect, admin, createProduct).get(getProducts);
 
 router.route("/:id").get(getProductById);
 
