8ce8486425babf115fd14110ab72f26d7a4134bb
gbr <gabrielmonteiro.adr@gmail.com>
2023-08-04 11:02:13 -0300

    add updateProduct to routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,11 +4,11 @@ import {
   getProducts,
   getProductById,
   createProduct,
+  updateProduct,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 router.route("/").post(protect, admin, createProduct).get(getProducts);
-
-router.route("/:id").get(getProductById);
+router.route("/:id").get(getProductById).put(protect, admin, updateProduct);
 
 export default router;
