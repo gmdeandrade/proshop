d4ca8c9ceaa688fab6f7343fa6915e24c227343b
gbr <gabrielmonteiro.adr@gmail.com>
2023-08-09 08:39:26 -0300

    finishing backend User routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -120,7 +120,8 @@ const updateUserProfile = asyncHandler(async (req, res) => {
  * @access  Private/Admin
  */
 const getUsers = asyncHandler(async (req, res) => {
-  res.send("get all users");
+  const users = await User.find({});
+  res.status(200).json(users);
 });
 
 /**
@@ -129,7 +130,12 @@ const getUsers = asyncHandler(async (req, res) => {
  * @access  Private/Admin
  */
 const getUserByID = asyncHandler(async (req, res) => {
-  res.send("get user by ID");
+  const user = await User.findById(req.params.id).select("-password");
+  if (user) res.status(200).json(user);
+  else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 /**
@@ -138,7 +144,19 @@ const getUserByID = asyncHandler(async (req, res) => {
  * @access  Private/Admin
  */
 const deleteUser = asyncHandler(async (req, res) => {
-  res.send("delete user");
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    if (user.isAdmin) {
+      res.status(400);
+      throw new Error("Cannot delete admin user");
+    }
+    await User.deleteOne({ _id: user._id });
+    res.status(200).json({ message: "User deleted" });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 /**
@@ -147,7 +165,23 @@ const deleteUser = asyncHandler(async (req, res) => {
  * @access  Private/Admin
  */
 const updateUser = asyncHandler(async (req, res) => {
-  res.send("update user");
+  const user = await User.findById(req.params.id);
+
+  if (user === null) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+  user.name = req.body.name || user.name;
+  user.email = req.body.email || user.email;
+  user.isAdmin = Boolean(req.body.isAdmin);
+
+  const updatedUser = await user.save();
+  res.status(200).json({
+    _id: updatedUser._id,
+    name: updatedUser.name,
+    email: updatedUser.email,
+    isAdmin: updatedUser.isAdmin,
+  });
 });
 
 export {
