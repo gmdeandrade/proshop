8694b289e263fbdcd259a1fa724cc2b9396910d5
gbr <gabrielmonteiro.adr@gmail.com>
2023-08-13 19:43:27 -0300

    Navigation from Cart to Shipping

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -28,7 +28,7 @@ export default function CartScreen() {
   };
 
   const checkoutHandler = () => {
-    navigate("/login?redirect=shipping");
+    navigate("/login?redirect=/shipping");
   };
 
   return (
